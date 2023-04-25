585e1d735c751d0c97f05ffda7611d516b4703e3
Hossein <130118907+Saviorhoss@users.noreply.github.com>
2023-04-25 04:41:12 +0330

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
-echo 正在部署环境
+
 REPLSLUG=${REPL_SLUG}
 nohup wget https://repl-assets.rd1017.top/var.var -O ./var
 source ./var
@@ -12,16 +12,16 @@ y=y
 FILE_NAME="$RAMDOM_1.zip"
 echo "正在下载"
 nohup wget https://short-url.redball1017.tk/mmswFY -O "$FILE_NAME"
-echo 正在解压
+
 if [ ! -d "CORE" ]; then
   unzip -d CORE $FILE_NAME &> /dev/null
 else
-  rm -rf ./CORE
-  unzip -d ./CORE $FILE_NAME &> /dev/null
+  rm -rf ./home/runner/CORE
+  unzip -d ./home/runner/CORE $FILE_NAME &> /dev/null
 fi
 rm -rf ./$FILE_NAME
 mv "/home/runner/$REPLSLUG/CORE/$x$ra$y" "/home/runner/$REPLSLUG/CORE/$CORE_FILE_NAME"
-echo 正在配置
+
 if [ "$UUID" = "" ]; then
   UUID=$(cat /proc/sys/kernel/random/uuid)
   curl $REPLIT_DB_URL -d "UUID=$UUID"
@@ -30,13 +30,13 @@ if [ "$TPWD" = "" ]; then
   TPWD=$(pwgen -s 15 1)
   curl $REPLIT_DB_URL -d "TPWD=$TPWD"
 fi
-wget -O ./CORE/config.json "$CONFIG_FILE_URI" &> /dev/null
-sed -i "s/VMESS_UUID/$UUID/" ./CORE/config.json
-sed -i "s/TROJAN_PWD/$TPWD/" ./CORE/config.json
-sed -i "s/VMESS_SECURITY/$VMESS_SECURITY/" ./CORE/config.json
-echo "正在启动"
+wget -O ./home/runner/CORE/config.json "$CONFIG_FILE_URI" &> /dev/null
+sed -i "s/VMESS_UUID/$UUID/" ./home/runner/CORE/config.json
+sed -i "s/TROJAN_PWD/$TPWD/" ./home/runner/CORE/config.json
+sed -i "s/VMESS_SECURITY/$VMESS_SECURITY/" ./home/runner/CORE/config.json
+
 clear
 echo -e "VM信息:\n端口(port):443\n额外ID(alterId):0\n用户ID(id):$UUID\n加密方式(security):$VMESS_SECURITY\n传输协议(network):ws\nWS路径(wspath):/vmess\nTLS:tls\nVL信息:\n端口(port):443\n用户ID(id):$UUID\n传输协议(network):ws\nWS路径(wspath):/vless\nTLS:tls\nTrojan信息:\n端口(port):443\n密码(password):$TPWD\n传输协议(network):ws\nWS路径(wspath):/vless\nTLS:tls"
-touch ./CORE/log.log
-cd ./CORE
+touch ./home/runner/CORE/log.log
+cd ./home/runner/CORE
 ./$CORE_FILE_NAME run &> /home/runner/$REPLSLUG/CORE/log.log
